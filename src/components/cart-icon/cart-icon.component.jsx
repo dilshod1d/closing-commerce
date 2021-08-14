34790e0c176680111433831374021ef4c474cd42
dilshod1d <76031536+dilshod1d@users.noreply.github.com>
2021-08-14 08:46:01 +0500

    Redux cart dropdown toggle implemented

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,12 @@
 import './cart-icon.styles.scss';
-import { AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineShopping, AiFillShopping } from 'react-icons/ai';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 const CartIcon = ({ toggleCartHidden }) => {
   return (
     <div className='cart-icon' onClick={toggleCartHidden}>
-      <AiOutlineShopping className='shopping-icon' />
+      <AiFillShopping className='shopping-icon' />
       <span className='item-count'>0</span>
     </div>
   );
